b0ee15f5f8c2b73ed3b87b98244138fd32ef5dd5
zoz0312 <zoz0312@naver.com>
2021-03-08 17:11:47 +0900

    nest party-manager module add & npm first setting

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Aurora
 Chat Bot Client
+
+## 운영 환경
+카카오톡 <=> Mobile <=> Aurora BackEnd <=> (추후 가능성)
+<hr>
+
+## Aurora Backend
+### 개요
+Mobile에서 전달받은 내용을 처리, 저장, 결과값 반환등과 같은 역할을 수행합니다.
--- aurora-backend/src/app.module.ts
+++ aurora-backend/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PartyManagerModule } from './party-manager/party-manager.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(),
+        DB_HOST: Joi.string(),
+      }),
+    }),
+    PartyManagerModule,
+    TypeOrmModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
