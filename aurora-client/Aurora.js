c5f0bd56587586324815b7d11e305a7edd899ddd
Chanmi Kim <chanmi_dev@naver.com>
2021-03-11 04:31:39 +0900

    feat: Create aurora-client

---
+++ aurora-client/Aurora.js
@@ -0,0 +1,53 @@
+const scriptName = "Aurora.js";
+
+/**
+ * (string) room
+ * (string) sender
+ * (boolean) isGroupChat
+ * (void) replier.reply(message)
+ * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
+ * (string) imageDB.getProfileBase64()
+ * (string) packageName
+ */
+function response (room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+    const resourceUrl = "http://zoz0312.com:8822";
+
+    if (room === "롤키웨이(LoLky Way)" || room === "오로라개발방"){
+    
+        let svcCd = "/user-custom-command";
+        if (msg.trim()[0] === "/"){
+            svcCd = "/command-manager";
+        }
+
+        const res = org.jsoup.Jsoup.connect(resourceUrl + svcCd)
+            .data('room', room)
+            .data('msg', msg.trim())
+            .data('sender', sender.split('/')[0])
+            .data('isGroupChat', isGroupChat)
+            .ignoreHttpErrors(true)
+            .ignoreContentType(true)
+            .post()
+            .outputSettings().prettyPrint(false);
+
+        replier.reply(res);
+
+    }
+}
+
+/**
+ * 아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
+ */
+function onCreate(savedInstanceState, activity) {
+    var textView = new android.widget.TextView(activity);
+    textView.setText("Hello, World!");
+    textView.setTextColor(android.graphics.Color.DKGRAY);
+    activity.setContentView(textView);
+}
+
+function onStart(activity) {}
+
+function onResume(activity) {}
+
+function onPause(activity) {}
+
+function onStop(activity) {}
